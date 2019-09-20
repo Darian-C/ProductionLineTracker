af1b192f4f2fecd48f6f7dbe1ae4b21cba9d6c8f
dcolon8953 <Dcolon8953@eagle.fgcu.edu>
2019-09-20 17:27:35 -0400

    Revised Project

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,23 @@
 package sample;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.text.Text;
 
 public class Controller {
+
+    @FXML
+    private Button btnClick;
+
+    @FXML
+    private Text outputText;
+
+    @FXML
+    public void display(ActionEvent event) {
+        if(event.getSource()==btnClick){
+            System.out.println("Clicked");
+        }
+    }
+
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,23 +1,41 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
+/**
+ * @author Darian Colon
+ */
 public class Main extends Application {
+    /**
+     * The starting point of a javaFX program
+     *
+     * @param primaryStage
+     * @return Exception
+     */
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception {
+        Controller cObject = new Controller();
+
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("Production Line Tracker");
+        primaryStage.setScene(new Scene(root, 600, 400));
         primaryStage.show();
+
     }
+
 
 
     public static void main(String[] args) {
         launch(args);
     }
+
 }
