a65c05dd792ac573f64e2ce786746a6f3177e0ad
dcolon8953 <Dcolon8953@eagle.fgcu.edu>
2019-09-25 18:06:45 -0400

    Revised Project

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -4,8 +4,16 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.text.Text;
-
+/**
+ * @author Darian Colon
+ */
 public class Controller {
+    /**
+     * Button action control
+     *
+     * @param primaryStage
+     * @return Exception
+     */
 //How do i print to the screen after the button in the Product Line tab
 //called Add Product is clicked.
     @FXML
@@ -16,9 +24,7 @@ public class Controller {
 
     @FXML
     public void display(ActionEvent event) {
-        if(event.getSource()==btnClick){
-            System.out.println("Clicked");
+        System.out.println("Clicked");
         }
     }
 
-}
