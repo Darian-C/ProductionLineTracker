720e9c23d5b59a143f280a5ff9408ad3fc4a1370
dcolon8953 <Dcolon8953@eagle.fgcu.edu>
2019-10-04 11:59:47 -0400

    Revised Project

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -3,7 +3,9 @@ import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.text.Text;
+import java.sql.*;
 /**
  * @author Darian Colon
  */
@@ -13,9 +15,10 @@ public class Controller {
      *
      * @param primaryStage
      * @return Exception
-     */
-//How do i print to the screen after the button in the Product Line tab
-//called Add Product is clicked.
+     *///JDBC = Java Database Connectivity
+    //Import Package -> Load and Register JDBC Driver ->
+    //Establish Connection -> Create Statement -> Execute Query ->
+    //Process Result -> Close
     @FXML
     private Button btnClick;
 
@@ -23,8 +26,54 @@ public class Controller {
     private Text outputText;
 
     @FXML
+    private ChoiceBox Choices;
+
+    @FXML
     public void display(ActionEvent event) {
-        System.out.println("Clicked");
+
+        // JDBC driver name and database URL
+        final String JDBC_DRIVER = "org.h2.Driver";
+        final String DB_URL = "jdbc:h2:./res/HR";
+
+        final String USER = "";
+        final String PASS = "";
+
+        Connection connect = null;
+        Statement stmt = null;
+
+        try {
+            // STEP 1: Register JDBC driver
+            Class.forName(JDBC_DRIVER);
+            //STEP 2: Open a connection to database
+            System.out.println("Connecting to database....");
+            connect = DriverManager.getConnection(DB_URL, USER, PASS);
+            System.out.println("Connected!");
+
+            //STEP 3:Statements/Prepared Statement/Calloble Statement
+            System.out.println("Inserting...");
+            stmt = connect.createStatement();
+
+            //String sqlItems = "INSERT INTO Product" + "VALUES ('AUDIO', 'Apple', 'iPod')";
+            stmt.executeUpdate("INSERT INTO Product" + "VALUES ('AUDIO', 'Apple', 'iPod')");
+            //PreparedStatement preparedItem = connect.prepareStatement(sqlItems);
+            //preparedItem.setString(1, "Audio");
+            //preparedItem.setString(2, "Apple");
+            //preparedItem.setString(3, "iPod");
+            //preparedItem.executeUpdate();
+
+
+            System.out.println("Inserted new information.");
+
+            //STEP 5: close
+            //preparedItem.close();
+            stmt.close();
+            connect.close();
+
+
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
         }
     }
+}
+
 
