dbecbdee70ad3c583654f8366be4c75246208c90
dcolon8953 <Dcolon8953@eagle.fgcu.edu>
2019-11-01 12:16:36 -0400

    Revised Project: Updated to meet sprint 2 objectives.

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,15 +1,10 @@
 package sample;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.stage.Stage;
-
-import java.util.Date;
 
 /** Allows user to add products to a table/chart.
  * @author Darian Colon
@@ -26,7 +21,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception {
         Controller cObject = new Controller();
 
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("ProductLayout.fxml"));
         primaryStage.setTitle("Production Line Tracker");
         primaryStage.setScene(new Scene(root, 600, 400));
         primaryStage.show();
